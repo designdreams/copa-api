6e8cc1e87ecc8d9e948d23c76398bdcebad894ea
designdreams <44630722+designdreams@users.noreply.github.com>
2019-10-02 09:32:50 -0700

    Added origin to test the CORS issue

--- src/main/java/com/designdreams/copass/service/PingService.java
+++ src/main/java/com/designdreams/copass/service/PingService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class PingService {
 
     @RequestMapping("/ping")
