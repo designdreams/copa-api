4c21ad14b73218667664cf006bbd97b4cae01c45
Saravana <saravanan.ganapathy@gmail.com>
2019-12-24 10:42:13 -0700

    Adding Notes to Add Trip page

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,6 +1,6 @@
-//var HOST = 'http://localhost:8080/';
+var HOST = 'http://localhost:8080/';
 // HOST= 'https://dd-project-c.appspot.com/';
- var HOST= 'http://www.copayana.com/';
+ //var HOST= 'http://www.copayana.com/';
 
 
   function signOut() {
@@ -113,7 +113,7 @@ hcol_airways.appendChild(document.createTextNode("Airways"));
 hcol_with.appendChild(document.createTextNode("With"));
 
 if(action == 'FIND'){
-hcol_email.appendChild(document.createTextNode("Email"));
+hcol_email.appendChild(document.createTextNode("Contact"));
 }
 else{
 tripHead.appendChild(document.createTextNode("Your Planned Trips"));
@@ -169,16 +169,72 @@ for (var i = 0; i < len; ++i) {
 
   if(action == 'FIND'){
   col_email.class = "coll";
+//<a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?">Dismissible popover</a>
     }
 
   col_from.appendChild(document.createTextNode(obj.tripList[i].source));
   col_to.appendChild(document.createTextNode(obj.tripList[i].destination));
-  col_date.appendChild(document.createTextNode(obj.tripList[i].travelStartDate));
-  col_airways.appendChild(document.createTextNode(obj.tripList[i].airways));
+
+  var date = "";
+  if(obj.tripList[i].travelStartDate == null ||
+  obj.tripList[i].travelStartDate == ""){
+  date = "Not Planned";
+  }else{
+  date = obj.tripList[i].travelStartDate;
+  }
+
+  col_date.appendChild(document.createTextNode(date));
+
+    var airways = "";
+    if(obj.tripList[i].airways == null ||
+    obj.tripList[i].airways == ""){
+    airways = "Any";
+    }else{
+    airways = obj.tripList[i].airways;
+    }
+
+  col_airways.appendChild(document.createTextNode(airways));
   col_travellingWith.appendChild(document.createTextNode(obj.tripList[i].travellingWith));
 
   if(action == 'FIND'){
-  col_email.appendChild(document.createTextNode(obj.tripList[i].travellerId));
+  //col_email.appendChild(document.createTextNode(obj.tripList[i].travellerId));
+
+    var detail = document.createElement('a');
+
+
+    var title = document.createAttribute("title");
+    title.value = obj.tripList[i].travellerId;
+
+     var href = document.createAttribute("href");
+        href.value = "#";
+
+        var id = document.createAttribute("id");
+                id.value = "myBtn";
+
+    var onclick = document.createAttribute("onclick");
+       // onclick.value = "openInfo(); return false;";
+       var contact_info = "Email Me :: "+obj.tripList[i].travellerId;
+         onclick.value = "modalblock('"+contact_info+"'); return false;"
+
+    var datatoggle = document.createAttribute("data-toggle");
+
+        datatoggle.value = "popover";
+
+    var datacontent = document.createAttribute("data-content");
+        datacontent.value = title.value;
+
+
+    detail.setAttributeNode(title);
+    detail.setAttributeNode(onclick);
+    detail.setAttributeNode(href);
+    detail.setAttributeNode(id);
+
+    //detail.setAttributeNode(datatoggle);
+   // detail.setAttributeNode(datacontent);
+
+    detail.appendChild(document.createTextNode("Info"));
+   col_email.appendChild(detail);
+
    }
 
     row.appendChild(col_from);
@@ -210,10 +266,15 @@ listDiv.appendChild(table);
 //</tbody></table></div>
 
 
+function openInfo(){
+alert("hello");
+//alert(this)
+}
+
 function findTrip(){
 
-var source = document.getElementById("src").value;
-var destination = document.getElementById("dest").value;
+var source = document.getElementById("src").value.substring(0, 3);
+var destination = document.getElementById("dest").value.substring(0, 3);
 var date = document.getElementById("trip-date").value;
 
 var data = JSON.stringify({
@@ -270,15 +331,15 @@ xhr.send(data);
 
 function addTrip(id){
 
-var source = document.getElementById("trip-from").value;
-var destination = document.getElementById("trip-to").value;
+var source = document.getElementById("trip-from").value.substring(0, 3);
+var destination = document.getElementById("trip-to").value.substring(0, 3);
 var date = document.getElementById("trip-date").value;
 var airways = document.getElementById("airways").value;
 var tripWith = document.getElementById("trip-with").value;
 var isTicketBooked = document.getElementById("trip-box-0").checked;
 var isFinalDestination = document.getElementById("trip-box-1").checked;
 var canTakePackageInd = document.getElementById("trip-box-2").checked;
-
+var notes = document.getElementById('notes').value;
 
 var data = JSON.stringify({
     "trip": {
@@ -292,7 +353,8 @@ var data = JSON.stringify({
         "isTicketBooked": isTicketBooked,
         "isDomestic": false,
         "canTakePackageInd": canTakePackageInd,
-        "isFinalDestination": isFinalDestination
+        "isFinalDestination": isFinalDestination,
+        "notes": notes
     }
 	});
 
@@ -301,9 +363,9 @@ xhrr.withCredentials = true;
 
 xhrr.addEventListener("readystatechange", function () {
 
-  if (this.readyState > 0) {
-   document.getElementById("add-trip-result").innerHTML = 'Please wait! Pulling trips.....';
-    }
+//  if (this.readyState > 0) {
+//   document.getElementById("add-trip-result").innerHTML = 'Please wait! Pulling trips.....';
+//    }
 
   if (this.readyState === 4) {
 
@@ -312,6 +374,7 @@ xhrr.addEventListener("readystatechange", function () {
 
     if(response) {
         var obj = JSON.parse(response);
+//           document.getElementById("add-trip-result").innerHTML = '';
            populateResult(obj);
     }else{
         document.getElementById("trips").innerHTML = 'SORRY! PLEASE TRY LATER!';
@@ -335,34 +398,97 @@ xhrr.send(data);
 
 function populateResult(obj){
 
-var listDiv = document.getElementById("add-trip-result");
+//var listDiv = document.getElementById("add-trip-result");
 
 if(obj && obj.respCode == "SUCCESS"){
-listDiv.innerHTML = "Great! Your trip is added successfully!";
+
+//listDiv.innerHTML = "Great! Your trip is added successfully!";
+openAddResponseModal("Great! Your trip is added successfully!");
+
 }else if(obj && obj.respCode == "INVALID_SAME_DAY_DUPLICATE_TRIP"){
-listDiv.innerHTML = "Trip already added!!";
+
+//listDiv.innerHTML = ;
+openAddResponseModal("Trip already added!!");
 
 }else if(obj && obj.respCode == "DUPLICATE_TRIP"){
- listDiv.innerHTML = "Trip already added!!";
+
+ //listDiv.innerHTML = "Trip already added!!";
+openAddResponseModal("Trip already added!!");
 
  }else{
-listDiv.innerHTML = "Could not add Trip. Please Try Again!";
+//listDiv.innerHTML = "Could not add Trip. Please Try Again!";
+openAddResponseModal("Could not add Trip. Please Try Again!");
 
 }
 
 }
 
 
-//function authcon(){
-//
-//alert('see');
-//document.getElementById("authcon-form").submit();
+
+/* contact info modal */
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+function modalblock(email){
+modal = document.getElementById("myModal");
+//span = document.getElementsByClassName("close")[0];
+span = document.getElementById("modal-close");
+document.getElementById("contact-info").innerHTML = email;
+  modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+function closemodal(){
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+//window.onclick = function(event) {
+//  if (event.target == modal || event.target == response_modal) {
+//    modal.style.display = "none";
+//  }
 //}
-//
-//$('#sandbox-container .input-group.date').datepicker({
-//});
 
-//$('.datepicker').datepicker({
-//    format: 'mm/dd/yyyy',
-//    startDate: '-3d'
-//});
+/* add trip result  modal */
+
+// Get the modal
+var response_modal ="";
+
+// Get the button that opens the modal
+//var btn = document.getElementById("response-modal-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openAddResponseModal(addResponse) {
+  response_modal = document.getElementById("myModal");
+  document.getElementById("add-response-info").innerHTML = addResponse;
+  response_modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeAddResponseModal() {
+  response_modal.style.display = "none";
+}
+
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal || event.target == response_modal) {
+  if(response_modal.style){
+      response_modal.style.display = "none";
+  }
+
+    if(modal.style){
+        modal.style.display = "none";
+    }
+
+  }
+}
